5c70894dd6c1d93536d422d46af9c2c741f812fd
unknown <alamgirhossaindrubo@gmail.com>
2025-01-06 10:48:23 +0600

    added jetstream package for creating admin

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,13 @@ Route::get('/customer/login-signup',[CustomerAuthController::class,'index'])->na
 Route::get('/all-blogs',[BlogController::class,'index'])->name('blog.index');
 
 Route::get('/product',[ProductController::class,'index'])->name('product.index');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
